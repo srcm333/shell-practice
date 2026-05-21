3b5ac0f375a07edc17590bbe72b4e47dc755716b
srcm333 <srcm333@gmail.com>
2026-05-21 23:55:20 +0530

    conversation

--- 02_conversation.sh
+++ 02_conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 
-PERSON1=Trump
-PERSON2=Iran
+PERSON1=Russia
+PERSON2=Ukrain
 echo "$PERSON1: Hey Iran, we are going to blast you!"
 echo "$PERSON2: Yes, Please. you are welcome"
 echo "$PERSON1: Hey $PERSON2 I am serious"
