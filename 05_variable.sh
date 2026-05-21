e449e2f921741da4894e9d300aef5650a31f71a5
srcm333 <srcm333@gmail.com>
2026-05-22 00:21:34 +0530

    conversation

--- 05_variable.sh
+++ 05_variable.sh
@@ -1,5 +1,15 @@
 #/bin/bash
 
-TIMESTAMP=$(date)
+#TIMESTAMP=$(date)
 
-echo "Time is: $TIMESTAMP"
+#echo "Time is: $TIMESTAMP"
+
+START_TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "script executed in $TOTAL_TIME seconds"
