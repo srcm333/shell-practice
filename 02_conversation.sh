320c0467f1212e8bb1053a0d576454453d883774
srcm333 <srcm333@gmail.com>
2026-05-21 23:46:37 +0530

    conversation

---
+++ 02_conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+echo "Trump: Hey Iran, we are going to blast you!"
+echo "Iran: Yes, Please. you are welcome"
+echo "Trump: Hey Iran I am serious"
+echo "Iran: Hey Trump, we are also serious"
