e86b760c5fd723cae4067bc65c60a30ccd427cd2
srcm333 <srcm333@gmail.com>
2026-05-22 00:01:54 +0530

    conversation

---
+++ 03_variavles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hey $PERSON2, we are going to blast you!"
+echo "$PERSON2: Yes, Please. you are welcome"
+echo "$PERSON1: Hey $PERSON2 I am serious"
+echo "$PERSON2: Hey $PERSON1, we are also serious"
