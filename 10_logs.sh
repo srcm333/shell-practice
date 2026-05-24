4ded477adf5f4a210b7f4e2be16984475c8ecfa1
srcm333 <srcm333@gmail.com>
2026-05-24 22:52:31 +0530

    install

---
+++ 10_logs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log  #/home/ec2-user/shell-logs/10_logs.sh.log
+
+#check root access or not
+if [ $USERID -ne 0 ]; then
+     echo "Please run this script with root access"
+     exit 1
+     
+fi
+
+# first arg  -> what are you trying to install
+# second arg  -> exit code
+VALIDATE(){
+      if [ $2 -ne 0]; then
+      echo "Installing $1 is ... Failed"
+      exit 1
+    else
+      echo "Installing $1 is ... SUCCESS"
+    fi   
+}
+
+dnf list installed mysql &>> $LOGS_FILE
+
+if [ $? -eq 0 ]; then
+     echo "MySQL is already installed ... SKIPPING"
+else
+     echo "Installing MySQL"
+     dnf install mysql -y
+     VALIDATE MySQL $?
+fi
+
+dnf list installed nginx &>> $LOGS_FILE
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING"
+else
+    echo "Installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $?
+fi
+
