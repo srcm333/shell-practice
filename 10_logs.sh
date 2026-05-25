980b9927f0b66072e79c09218fb896a2c8aebe70
srcm333 <srcm333@gmail.com>
2026-05-25 21:46:40 +0530

    logs

--- 10_logs.sh
+++ 10_logs.sh
@@ -25,7 +25,7 @@ VALIDATE(){
 dnf list installed mysql &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-     echo "MySQL is already installed ... SKIPPING"
+     echo "MySQL is already installed ... SKIPPING" | tee -a $LOGS_FILE
 else
      echo "Installing MySQL"
      dnf install mysql -y &>> $LOGS_FILE
@@ -34,7 +34,7 @@ fi
 
 dnf list installed nginx &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
-    echo "nginx is already installed ... SKIPPING"
+    echo "nginx is already installed ... SKIPPING" | tee -a $LOGS_FILE
 else
     echo "Installing nginx"
     dnf install nginx -y &>> $LOGS_FILE
