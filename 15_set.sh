9b9bb60f4358451a68450f57fccbda08311b4b3b
srcm333 <srcm333@gmail.com>
2026-05-25 22:54:23 +0530

    logs

--- 15_set.sh
+++ 15_set.sh
@@ -11,6 +11,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+trap 'echo "error at $LINENO", command: $BASH_COMMAND"'
+
 #check root access or not
 if [ $USERID -ne 0 ]; then
      echo "Please run this script with root access"
