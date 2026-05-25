650e098f32fb9e488c5e692c02db1f1cc886cc2d
srcm333 <srcm333@gmail.com>
2026-05-25 21:31:22 +0530

    logs

--- 10_logs.sh
+++ 10_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 
 USERID=$(id -u)
-LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"  # /home/ec2-user/shell-logs/10_logs.sh.log
 
 # check root access or not
