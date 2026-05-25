b96810dfc01fa525d2eee05c1534b9a33e901252
srcm333 <srcm333@gmail.com>
2026-05-25 22:40:41 +0530

    logs

---
+++ 13_colour.sh
@@ -0,0 +1,42 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#check root access or not
+if [ $USERID -ne 0 ]; then
+     echo "Please run this script with root access"
+     exit 1
+     
+fi
+
+# first arg  -> what are you trying to install
+# second arg  -> exit code
+VALIDATE(){
+      if [ $2 -ne 0]; then
+      echo "$TIMESTAMP [ERROR] Installing $1 is ... $R Failed $N " |tee -a $LOGS_FILE
+      exit 1
+    else
+      echo "$TIMESTAMP [INFO] Installing $1 is ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi   
+}
+
+for package in $@
+do
+    echo "$TIMESTAMP [INFO] Installing $package"
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE "Installing $package" $?
+    else
+       echo "$TIMESTAMP [INFO] $package already installed  ..... $Y SKIPPING $N"
+    fi
+done
+
+
