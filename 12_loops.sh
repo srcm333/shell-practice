372ec281c3076cae357cb546b71728490961f83b
srcm333 <srcm333@gmail.com>
2026-05-25 21:57:58 +0530

    logs

---
+++ 12_loops.sh
@@ -0,0 +1,30 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+#check root access or not
+if [ $USERID -ne 0 ]; then
+     echo "Please run this script with root access"
+     exit 1
+     
+fi
+
+# first arg  -> what are you trying to install
+# second arg  -> exit code
+VALIDATE(){
+      if [ $2 -ne 0]; then
+      echo "Installing $1 is ... Failed"
+      exit 1
+    else
+      echo "Installing $1 is ... SUCCESS"
+    fi   
+}
+
+for package in $@
+do
+    echo "Installing $package
+done
+
+
